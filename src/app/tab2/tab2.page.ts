500a8c426fb9effcdfbc2852cddf4dbe899c5b2c
raj-pwr <pawar@partikel-art.de>
2025-02-19 00:47:51 +0100

    Add back button to movie search modal and implement movie detail modal opening functionality

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 import { RecommendationsResult } from 'src/models/movie-details.model';
 import { MovieService } from 'src/services/movie.service';
+import { MovieDetailModalComponent } from '../movie-detail-modal/movie-detail-modal.component';
 
 @Component({
   selector: 'app-tab2',
@@ -10,9 +12,31 @@ import { MovieService } from 'src/services/movie.service';
 })
 export class Tab2Page {
 
-  constructor(private movieService: MovieService) {}
+  constructor(
+    private movieService: MovieService,
+    private modalCtrl: ModalController
+  ) { }
 
   get watchedMovies(): RecommendationsResult[] {
     return this.movieService.watched;
+  }
+
+  // 3) Method to open the modal
+  async openMovieDetail(movieId: number) {
+    const modal = await this.modalCtrl.create({
+      component: MovieDetailModalComponent,
+      componentProps: { movieId }
+    });
+    await modal.present();
+
+    // Optionally handle the data returned on dismiss
+    const { data } = await modal.onDidDismiss();
+    if (data?.action === 'watchlist') {
+      console.log('User added movie to watchlist:', data.movie.title);
+      // If you want to do something with the watchlist result, do it here
+    } else if (data?.action === 'watched') {
+      console.log('User marked movie as watched:', data.movie.title);
+      // If you want to handle that event, do it here
+    }
   }
 }
