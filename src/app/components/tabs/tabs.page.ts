9882043800b04b661ddaa580118857a76faf8e4e
r-a-j <er.rajpawar@gmail.com>
2025-03-23 22:35:32 +0100

    Add search modal button and implement watchlist loading in tabs component

--- src/app/components/tabs/tabs.page.ts
+++ src/app/components/tabs/tabs.page.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ModalController } from '@ionic/angular';
+import { MovieSearchModalComponent } from '../movie-search-modal/movie-search-modal.component';
+import { MovieDetails } from 'src/app/models/movie-details.model';
+import { MovieService } from 'src/app/services/movie.service';
 
 @Component({
   selector: 'app-tabs',
@@ -7,11 +11,31 @@ import { Router } from '@angular/router';
   styleUrls: ['tabs.page.scss'],
   standalone: false,
 })
-export class TabsPage {
-
+export class TabsPage implements OnInit {
   activeTab: string = 'tab1';
+  watchlist: MovieDetails[] = [];
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private modalController: ModalController,
+    private movieService: MovieService,
+  ) { }
+
+  async ngOnInit() {
+    await this.loadWatchlist();
+  }
+
+  async loadWatchlist() {
+    this.watchlist = await this.movieService.getWatchlist();
+  }
+
+  async openSearchModal() {
+    const modal = await this.modalController.create({
+      component: MovieSearchModalComponent,
+      componentProps: { watchlist: this.watchlist }
+    });
+    return await modal.present();
+  }
 
   openSettings() {
     this.router.navigate(['/tabs/settings']);
