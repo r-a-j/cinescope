5dac4f76cdf7f616772d57891ab7931041f12934
Raj Pawar <raj24100@outlook.com>
2025-04-25 18:26:18 +0530

    Refactor actor components

--- src/app/discover-actors/discover-actors.component.ts
+++ src/app/discover-actors/discover-actors.component.ts
@@ -1,43 +1,13 @@
-import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { IonSkeletonText } from "@ionic/angular/standalone";
-import { PersonResult } from 'src/models/person.model';
-import { TmdbSearchService } from 'src/services/tmdb-search.service';
+import { Component } from '@angular/core';
+import { DiscoverActorsPopularComponent } from '../discover-actors-popular/discover-actors-popular.component';
 
 @Component({
   selector: 'app-discover-actors',
   templateUrl: './discover-actors.component.html',
   styleUrls: ['./discover-actors.component.scss'],
-  imports: [IonSkeletonText, CommonModule],
+  imports: [DiscoverActorsPopularComponent],
   standalone: true,
 })
-export class DiscoverActorsComponent implements OnInit {
-
-  popularPersons: PersonResult[] = [];
-  isLoadingPersons = true;
-
-  constructor(
-    public router: Router,
-    private tmdbService: TmdbSearchService,
-  ) { }
-
-  ngOnInit() {
-    this.loadPopularPersons();
-  }
-
-  loadPopularPersons(): void {
-    this.tmdbService.getPopularPersons(1).subscribe({
-      next: (response) => {
-        this.popularPersons = response.results;
-        this.isLoadingPersons = false;
-      },
-      error: (error) => console.error(error)
-    });
-  }
-
-  navigateToPersonDetail(id: number): void {
-    this.router.navigate(['/person-detail', id]);
-  }
-
+export class DiscoverActorsComponent {
+  constructor() { }
 }
